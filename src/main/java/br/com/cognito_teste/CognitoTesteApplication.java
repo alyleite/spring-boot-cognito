192cfc39e78af442a5452e596b4fb9f76c7de458
alyson <a.xavier@nextage.com.br>
2024-08-10 11:38:54 -0300

    Commit inicial

---
+++ src/main/java/br/com/cognito_teste/CognitoTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.cognito_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoTesteApplication.class, args);
+	}
+
+}
